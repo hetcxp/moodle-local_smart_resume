ced943963fc2206514c8bcefb2a545d4da95586b
hetcxp <hetcxp@gmail.com>
2026-06-10 10:55:10 -0300

    Fix Moodle requires version format

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_smart_resume';
 $plugin->version   = 2026061000;
-$plugin->requires  = 2025110000; // Moodle 5.2 (aprox)
+$plugin->requires  = 2025110100; // Moodle 5.2 (aprox - YYYYMMDDRR)
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.1';
